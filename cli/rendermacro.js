d771c5a54a994c94874c552ffa957cfee7e69dd9
pac-dev <pierre@osar.fr>
2024-02-20 11:13:51 -0700

    Whitespace
    Yes. No one cares.

--- cli/rendermacro.js
+++ cli/rendermacro.js
@@ -4,44 +4,44 @@ import { path } from './deps.js';
 
 export const renderMacro = async (projDir, macroPath, outPath) => {
 
-    const content = await Deno.readTextFile(macroPath);
-    const lines = content.split('\n');
-    const r = new MultiRenderer();
+	const content = await Deno.readTextFile(macroPath);
+	const lines = content.split('\n');
+	const r = new MultiRenderer();
 
-    // i think the web version reuses params from oldPatch
-    // todo make this more consistent between versions
-    macroEvents.startPatch = (patchName, params, oldPatch) => {
-        const paramObj = {};
-        for (const p of params) paramObj[p.name] = p.valStr;
-        const mainPath = path.join(projDir, patchName, 'main.js');
-        return r.addPatch(mainPath, paramObj);
-    };
-    macroEvents.tweakPatch = (cmdPatch, param) => {
-        if (!cmdPatch) throw new Error('no such patch.');
-        r.tweakPatch(cmdPatch, { [param.name]: param.valStr });
-    };
-    macroEvents.stopPatch = (cmdPatch) => {
-        if (!cmdPatch) throw new Error('no such patch.');
-        r.removePatch(cmdPatch);
-    };
-    macroEvents.setHighlight = (file, lineNum) => {
-        console.log(lines[lineNum-1]);
-    };
-    console.log('\nrendering track: ' + path.basename(macroPath));
+	// i think the web version reuses params from oldPatch
+	// todo make this more consistent between versions
+	macroEvents.startPatch = (patchName, params, oldPatch) => {
+		const paramObj = {};
+		for (const p of params) paramObj[p.name] = p.valStr;
+		const mainPath = path.join(projDir, patchName, 'main.js');
+		return r.addPatch(mainPath, paramObj);
+	};
+	macroEvents.tweakPatch = (cmdPatch, param) => {
+		if (!cmdPatch) throw new Error('no such patch.');
+		r.tweakPatch(cmdPatch, { [param.name]: param.valStr });
+	};
+	macroEvents.stopPatch = (cmdPatch) => {
+		if (!cmdPatch) throw new Error('no such patch.');
+		r.removePatch(cmdPatch);
+	};
+	macroEvents.setHighlight = (file, lineNum) => {
+		console.log(lines[lineNum-1]);
+	};
+	console.log('\nrendering track: ' + path.basename(macroPath));
 	const perfStart = performance.now();
-    const outHandle = await r.addOutput(outPath);
-    let nowStamp = 0;
-    offlineInit();
-    while (true) {
-        const nextStamp = playUpToStamp(content, nowStamp);
-        if (nextStamp === Number.MAX_VALUE) {
-            await r.removeOutput(outHandle);
-            break;
-        }
-        await r.render((nextStamp - nowStamp)/1000);
-        nowStamp = nextStamp;
-    }
-    const perfTime = performance.now() - perfStart;
-    console.log('done. speed: '+(Math.round(100*nowStamp/perfTime)/100)+'x');
-    await r.finalize();
+	const outHandle = await r.addOutput(outPath);
+	let nowStamp = 0;
+	offlineInit();
+	while (true) {
+		const nextStamp = playUpToStamp(content, nowStamp);
+		if (nextStamp === Number.MAX_VALUE) {
+			await r.removeOutput(outHandle);
+			break;
+		}
+		await r.render((nextStamp - nowStamp)/1000);
+		nowStamp = nextStamp;
+	}
+	const perfTime = performance.now() - perfStart;
+	console.log('done. speed: '+(Math.round(100*nowStamp/perfTime)/100)+'x');
+	await r.finalize();
 }
--- core/faustCompiler.js
+++ core/faustCompiler.js
@@ -3,25 +3,25 @@ import Module from './libfaust-wasm.js';
 let libFaust, em, faustPromise;
 const getFaustPromise = () => new Promise(resolve => {
 	console.log('Initializing Faust compiler.');
-    Module().then(mod => {
-        let ret = {};
-        ret.createWasmCDSPFactoryFromString = mod.cwrap("createWasmCDSPFactoryFromString", "number", ["number", "number", "number", "number", "number", "number"]);
-        ret.deleteAllWasmCDSPFactories = mod.cwrap("deleteAllWasmCDSPFactories", null, []);
-        ret.expandCDSPFromString = mod.cwrap("expandCDSPFromString", "number", ["number", "number", "number", "number", "number", "number"]);
-        ret.getCLibFaustVersion = mod.cwrap("getCLibFaustVersion", "number", []);
-        ret.getWasmCModule = mod.cwrap("getWasmCModule", "number", ["number"]);
-        ret.getWasmCModuleSize = mod.cwrap("getWasmCModuleSize", "number", ["number"]);
-        ret.getWasmCHelpers = mod.cwrap("getWasmCHelpers", "number", ["number"]);
-        ret.freeWasmCModule = mod.cwrap("freeWasmCModule", null, ["number"]);
-        ret.freeCMemory = mod.cwrap("freeCMemory", null, ["number"]);
-        ret.cleanupAfterException = mod.cwrap("cleanupAfterException", null, []);
-        ret.getErrorAfterException = mod.cwrap("getErrorAfterException", "number", []);
-        ret.getLibFaustVersion = () => mod.UTF8ToString(this.getCLibFaustVersion());
-        ret.generateCAuxFilesFromString = mod.cwrap("generateCAuxFilesFromString", "number", ["number", "number", "number", "number", "number"]);
-        em = mod;
-        libFaust = ret;
-        resolve(ret);
-    });
+	Module().then(mod => {
+		let ret = {};
+		ret.createWasmCDSPFactoryFromString = mod.cwrap("createWasmCDSPFactoryFromString", "number", ["number", "number", "number", "number", "number", "number"]);
+		ret.deleteAllWasmCDSPFactories = mod.cwrap("deleteAllWasmCDSPFactories", null, []);
+		ret.expandCDSPFromString = mod.cwrap("expandCDSPFromString", "number", ["number", "number", "number", "number", "number", "number"]);
+		ret.getCLibFaustVersion = mod.cwrap("getCLibFaustVersion", "number", []);
+		ret.getWasmCModule = mod.cwrap("getWasmCModule", "number", ["number"]);
+		ret.getWasmCModuleSize = mod.cwrap("getWasmCModuleSize", "number", ["number"]);
+		ret.getWasmCHelpers = mod.cwrap("getWasmCHelpers", "number", ["number"]);
+		ret.freeWasmCModule = mod.cwrap("freeWasmCModule", null, ["number"]);
+		ret.freeCMemory = mod.cwrap("freeCMemory", null, ["number"]);
+		ret.cleanupAfterException = mod.cwrap("cleanupAfterException", null, []);
+		ret.getErrorAfterException = mod.cwrap("getErrorAfterException", "number", []);
+		ret.getLibFaustVersion = () => mod.UTF8ToString(this.getCLibFaustVersion());
+		ret.generateCAuxFilesFromString = mod.cwrap("generateCAuxFilesFromString", "number", ["number", "number", "number", "number", "number"]);
+		em = mod;
+		libFaust = ret;
+		resolve(ret);
+	});
 });
 
 /**
@@ -39,69 +39,69 @@ const getFaustPromise = () => new Promise(resolve => {
  * @returns {Object} - ui8Code is wasm bytecode, helpersCode is C (?) code to extract metadata from
  */
 const compileCode = (factoryName, code, argv=[], internalMemory) => {
-    const codeSize = em.lengthBytesUTF8(code) + 1;
-    const $code = em._malloc(codeSize);
-    const name = "FaustDSP";
-    const nameSize = em.lengthBytesUTF8(name) + 1;
-    const $name = em._malloc(nameSize);
-    const $errorMsg = em._malloc(4096);
+	const codeSize = em.lengthBytesUTF8(code) + 1;
+	const $code = em._malloc(codeSize);
+	const name = "FaustDSP";
+	const nameSize = em.lengthBytesUTF8(name) + 1;
+	const $name = em._malloc(nameSize);
+	const $errorMsg = em._malloc(4096);
 
-    em.stringToUTF8(name, $name, nameSize);
-    em.stringToUTF8(code, $code, codeSize);
+	em.stringToUTF8(name, $name, nameSize);
+	em.stringToUTF8(code, $code, codeSize);
 
-    argv.push("-cn", factoryName);
-    argv.push("-I", "/libraries");
+	argv.push("-cn", factoryName);
+	argv.push("-I", "/libraries");
 
-    // Prepare 'argv_aux' array for C side
-    const ptrSize = 4;
-    const $argv = em._malloc(argv.length * ptrSize); // Get buffer from emscripten.
-    let argvBuffer$ = new Int32Array(em.HEAP32.buffer, $argv, argv.length); // Get a integer view on the newly allocated buffer.
-    for (let i = 0; i < argv.length; i++) {
-        const size$arg = em.lengthBytesUTF8(argv[i]) + 1;
-        const $arg = em._malloc(size$arg);
-        em.stringToUTF8(argv[i], $arg, size$arg);
-        argvBuffer$[i] = $arg;
-    }
-    try {
-        // const time1 = performance.now();
-        const $moduleCode = libFaust.createWasmCDSPFactoryFromString($name, $code, argv.length, $argv, $errorMsg, internalMemory);
-        // const time2 = performance.now();
-        // faustLog("Faust compilation duration : " + (time2 - time1));
-        const errorMsg = em.UTF8ToString($errorMsg);
-        if (errorMsg) throw new Error(errorMsg);
+	// Prepare 'argv_aux' array for C side
+	const ptrSize = 4;
+	const $argv = em._malloc(argv.length * ptrSize); // Get buffer from emscripten.
+	let argvBuffer$ = new Int32Array(em.HEAP32.buffer, $argv, argv.length); // Get a integer view on the newly allocated buffer.
+	for (let i = 0; i < argv.length; i++) {
+		const size$arg = em.lengthBytesUTF8(argv[i]) + 1;
+		const $arg = em._malloc(size$arg);
+		em.stringToUTF8(argv[i], $arg, size$arg);
+		argvBuffer$[i] = $arg;
+	}
+	try {
+		// const time1 = performance.now();
+		const $moduleCode = libFaust.createWasmCDSPFactoryFromString($name, $code, argv.length, $argv, $errorMsg, internalMemory);
+		// const time2 = performance.now();
+		// faustLog("Faust compilation duration : " + (time2 - time1));
+		const errorMsg = em.UTF8ToString($errorMsg);
+		if (errorMsg) throw new Error(errorMsg);
 
-        if ($moduleCode === 0) return null;
-        const $compiledCode = libFaust.getWasmCModule($moduleCode);
-        const compiledCodeSize = libFaust.getWasmCModuleSize($moduleCode);
-        // Copy native 'binary' string in JavaScript Uint8Array
-        const ui8Code = new Uint8Array(compiledCodeSize);
-        for (let i = 0; i < compiledCodeSize; i++) {
-            // faster than 'getValue' which gets the type of access for each read...
-            ui8Code[i] = em.HEAP8[$compiledCode + i];
-        }
-        const $helpersCode = libFaust.getWasmCHelpers($moduleCode);
-        const helpersCode = em.UTF8ToString($helpersCode);
-        // Free strings
-        em._free($code);
-        em._free($name);
-        em._free($errorMsg);
-        // Free C allocated wasm module
-        libFaust.freeWasmCModule($moduleCode);
-        // Get an updated integer view on the newly allocated buffer after possible emscripten memory grow
-        argvBuffer$ = new Int32Array(em.HEAP32.buffer, $argv, argv.length);
-        // Free 'argv' C side array
-        for (let i = 0; i < argv.length; i++) {
-            em._free(argvBuffer$[i]);
-        }
-        em._free($argv);
-        return { ui8Code, helpersCode };
-    } catch (e) {
-        // libfaust is compiled without C++ exception activated, so a JS exception is thrown and caught here
-        const errorMsg = em.UTF8ToString(libFaust.getErrorAfterException());
-        libFaust.cleanupAfterException();
-        // Report the Emscripten error
-        throw errorMsg ? new Error(errorMsg) : e;
-    }
+		if ($moduleCode === 0) return null;
+		const $compiledCode = libFaust.getWasmCModule($moduleCode);
+		const compiledCodeSize = libFaust.getWasmCModuleSize($moduleCode);
+		// Copy native 'binary' string in JavaScript Uint8Array
+		const ui8Code = new Uint8Array(compiledCodeSize);
+		for (let i = 0; i < compiledCodeSize; i++) {
+			// faster than 'getValue' which gets the type of access for each read...
+			ui8Code[i] = em.HEAP8[$compiledCode + i];
+		}
+		const $helpersCode = libFaust.getWasmCHelpers($moduleCode);
+		const helpersCode = em.UTF8ToString($helpersCode);
+		// Free strings
+		em._free($code);
+		em._free($name);
+		em._free($errorMsg);
+		// Free C allocated wasm module
+		libFaust.freeWasmCModule($moduleCode);
+		// Get an updated integer view on the newly allocated buffer after possible emscripten memory grow
+		argvBuffer$ = new Int32Array(em.HEAP32.buffer, $argv, argv.length);
+		// Free 'argv' C side array
+		for (let i = 0; i < argv.length; i++) {
+			em._free(argvBuffer$[i]);
+		}
+		em._free($argv);
+		return { ui8Code, helpersCode };
+	} catch (e) {
+		// libfaust is compiled without C++ exception activated, so a JS exception is thrown and caught here
+		const errorMsg = em.UTF8ToString(libFaust.getErrorAfterException());
+		libFaust.cleanupAfterException();
+		// Report the Emscripten error
+		throw errorMsg ? new Error(errorMsg) : e;
+	}
 }
 
 const factoryId = (() => {
